08805010460c013fd86ddd45fcf32cb53bc19fef
Peter Halloran <mkwenhua101@gmail.com>
2018-02-07 04:02:03 -0600

    config: configure redis for session store and for later things

--- config/passport.js
+++ config/passport.js
@@ -1,48 +1,44 @@
 const cors = require('cors');
 const passport = require('passport');
 const session = require('express-session');
-const cookieSession = require('cookie-session');
+const RedisStore = require('connect-redis')(session);
 const LocalStrategy = require('passport-local').Strategy;
-const {
-  SignupAuth,
-  SigninAuth,
-  DeserializeUser
-} = require('./auth');
+const {SignupAuth, SigninAuth, DeserializeUser} = require('./auth');
+const redis = require('redis');
+const client = redis.createClient(18121, 'redis-18121.c16.us-east-1-2.ec2.cloud.redislabs.com');
+const redisOptions = {
+  client: client
+}
+
+client.on('connect', () => console.log('Connected to Redis'));
 
 module.exports = (app) => {
-app.use(cookieSession({
-  name: 'session',
-  keys: ['baja', 'blast']
-}))
-app.use(session({secret: 'think outside the bun',
-resave: true,
-cookie: { secure: true },
-saveUninitialized: true}))
-app.use(passport.initialize());
-app.use(passport.session());
-app.set('trust proxy', 1);
-app.use(cors('*'));
+  app.use(session({store: new RedisStore(redisOptions), secret: 'think outside the bun', resave: true, saveUninitialized: true}))
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.set('trust proxy', 1);
+  app.use(cors('*'));
 
-const LocalSignupStrategy = new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password',
-  passReqToCallback: true
-}, SignupAuth)
+  const LocalSignupStrategy = new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, SignupAuth)
 
-const LocalSigninStrategy = new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password',
-  passReqToCallback: true
-}, SigninAuth)
+  const LocalSigninStrategy = new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, SigninAuth)
 
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
 
-passport.deserializeUser(DeserializeUser);
+  passport.deserializeUser(DeserializeUser);
 
-passport.use('local-signup', LocalSignupStrategy);
-passport.use('local-signin', LocalSigninStrategy);
+  passport.use('local-signup', LocalSignupStrategy);
+  passport.use('local-signin', LocalSigninStrategy);
 
   return passport;
 }
